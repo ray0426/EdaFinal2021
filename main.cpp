fad71b76c8c09bb55a73c3ecf2e6ab7e83c7592b
henry <b08901032@ntu.edu.tw>
2021-06-04 22:20:50 +0800

    pin

--- main.cpp
+++ main.cpp
@@ -6,66 +6,61 @@
 using namespace std;
 
 
+
+struct Pin {
+    int row;
+    int col;
+};
+struct Net2D {
+    vector<Pin> pin2Ds;
+    string name;
+    // string minRouteLayConst;
+    float weight;
+};
+
+vector<Net2D> pinThree2Two(Problem *pro);
+
 int main (int argc, char** argv) {
     Problem* pro = new Problem;
     pro->readCase(argv[1]);
-
+    vector<Net2D> flattenNet = pinThree2Two(pro);
     delete pro;
     return 0;
 }
 
-struct coordinate {
-    int row;
-    int col;
-    string layer;
-};
-struct Net2D {
-    vector<coordinate> pin2Ds;
-    string name;
-    string minRouteLayConst;
-    float weight;
-};
 
 vector<Net2D> pinThree2Two (Problem* pro) {  
     vector<Net2D> flatenNets;
+    CellInst currentCellinst;
+    string instName;
+    Net CurrentNet;
+    Pin pin;
 
+    //making ele of Net2D
     for (int i = 0; i < pro->NumNet; i++) {
-        //ctrlCV
-        Net CurrentNet = pro->nets[i];
+        CurrentNet = pro->nets[i];
+
         Net2D flatenNet;
         flatenNet.name = CurrentNet.name;
-        flatenNet.minRouteLayConst = CurrentNet.minRouteLayConst;
+        // flatenNet.minRouteLayConst = CurrentNet.minRouteLayConst;
         flatenNet.weight = CurrentNet.weight;
 
-        //
+        vector<Pin> pins;
+        
+        //finding flatenNet.pin2Ds
         for (int j = 0; j < CurrentNet.NumPins; j++) {
-            string instName = CurrentNet.pins[j].instName;
-            string masterPinName = CurrentNet.pins[j].masterPinName;
-
-            for (int k = 0; k < pro->NumCellInst; k++){
-                CellInst currentCellinst = pro->cellinsts[k];
+            instName = CurrentNet.pins[j].instName;
+            // string masterPinName = CurrentNet.pins[j].masterPinName;
+            for (auto currentCellinst : pro->cellinsts) {
                 if (instName == currentCellinst.name) {
-                    coordinate c;
-                    c.col = currentCellinst.colIdx;
-                    c.row = currentCellinst.rowIdx;
-                    for (int m = 0; m < pro->NumMasterCell; m++){
-                        MasterCell currentMasterCell = pro->masterCells[m];
-                        if (currentCellinst.MasterCellName == currentMasterCell.name) {
-                            for (int n = 0; n < currentMasterCell.pinCount; n++){
-                                MasterCellPin currentMasterCellPin = currentMasterCell.pins[n];
-                                if (currentMasterCellPin.name == masterPinName){
-                                    c.layer = currentMasterCellPin.layer;
-                                    break;
-                                }
-                            }
-                            break;
-                        }
-                    }
-                    flatenNet.pin2Ds.push_back(c);
+                    pin.col = currentCellinst.colIdx;
+                    pin.row = currentCellinst.rowIdx;
+                    pins.push_back(pin);
                     break;
                 }
             }
         }
+        flatenNet.pin2Ds = pins;
         flatenNets.push_back(flatenNet);
     }
 } 
