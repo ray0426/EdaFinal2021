2e90b5ba07af94ac63b581bee8646292ba275614
henry <b08901032@ntu.edu.tw>
2021-06-04 23:15:02 +0800

    supplygraph

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@
 using namespace std;
 
 
-
+struct EdgeSupply {
+    int row;  //upSupply
+    int col;  //leftSupply
+};
 struct Pin {
     int row;
     int col;
@@ -14,16 +17,19 @@ struct Pin {
 struct Net2D {
     vector<Pin> pin2Ds;
     string name;
-    // string minRouteLayConst;
     float weight;
 };
 
+vector<vector<EdgeSupply>> GenerateSupplyGraph(Problem *pro);
 vector<Net2D> pinThree2Two(Problem *pro);
 
 int main (int argc, char** argv) {
     Problem* pro = new Problem;
     pro->readCase(argv[1]);
+    //
     vector<Net2D> flattenNet = pinThree2Two(pro);
+    vector<vector<EdgeSupply>> SupplyGraph = GenerateSupplyGraph(pro);
+    //
     delete pro;
     return 0;
 }
@@ -35,17 +41,18 @@ vector<Net2D> pinThree2Two (Problem* pro) {
     string instName;
     Net CurrentNet;
     Pin pin;
-
+    Net2D flatenNet;
+    vector<Pin> pins;
     //making ele of Net2D
     for (int i = 0; i < pro->NumNet; i++) {
         CurrentNet = pro->nets[i];
 
-        Net2D flatenNet;
+        flatenNet.pin2Ds.clear();
         flatenNet.name = CurrentNet.name;
         // flatenNet.minRouteLayConst = CurrentNet.minRouteLayConst;
         flatenNet.weight = CurrentNet.weight;
 
-        vector<Pin> pins;
+        pins.clear();
         
         //finding flatenNet.pin2Ds
         for (int j = 0; j < CurrentNet.NumPins; j++) {
@@ -63,4 +70,40 @@ vector<Net2D> pinThree2Two (Problem* pro) {
         flatenNet.pin2Ds = pins;
         flatenNets.push_back(flatenNet);
     }
-} 
+    return flatenNets;
+}
+vector<vector<EdgeSupply>> GenerateSupplyGraph (Problem* pro) {
+    int hSupply;
+    int vSupply;
+    vector<vector<EdgeSupply>> graph;
+    vector<EdgeSupply> rowGraph;
+    EdgeSupply grid;
+
+    for (Layer layer: pro->layers) {
+        if (layer.direction == 'H')
+            hSupply += layer.defaultsupply;
+        else if (layer.direction == 'V')
+            vSupply += layer.defaultsupply;
+    }
+
+    for (int i = 0; i < pro->GGridBD[2]; i++) {
+        rowGraph.clear();
+        for (int j = 0; j < pro->GGridBD[3]; j++) {
+            grid.col = (i == 0) ? 0 : vSupply;
+            grid.row = (j == 0) ? 0 : hSupply;
+            rowGraph.push_back(grid);
+        }
+        graph.push_back(rowGraph);
+    }
+
+    for (NonDefaultSupply nd: pro->NonDefaultSupplies) {
+        for (Layer layer: pro->layers) {
+            if (layer.direction == 'H')
+                graph[nd.rowIdx][nd.colIdx].row += nd.relatedValue;
+            else if (layer.direction == 'V')
+                graph[nd.rowIdx][nd.colIdx].col += nd.relatedValue;
+            break;
+        }
+    }
+    return graph;
+}
