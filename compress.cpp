29f729c45b7e8440673c1c94bf1714bd9c64eb04
henry <b08901032@ntu.edu.tw>
2021-06-14 16:04:03 +0800

    modify(there might be error)

--- compress.cpp
+++ compress.cpp
@@ -6,6 +6,9 @@
 #include <algorithm>
 using namespace std;
 
+bool isPosSame (Pos a, Pos b) {
+    return (a.col == b.col && a.row == b.row);
+}
 vector<Net2D> Three2Two (Problem* pro) {
     vector<Net2D> flatenNets;   //return
 
@@ -124,68 +127,87 @@ TwoPinRoute2D Multi2TwoPinRoute (Net2D* net, Pos sPin, Pos ePin) {
     TwoPinNet.ePin = ePin;
     TwoPinNet.sPin = sPin;      //setting
     Route2D line;               //buffer
-    
+    vector<Route2D>* ref;
+    *ref = net->route2Ds;
+
     //return with ePin to sPin
-    vector<Pos> rawRoute = FindRoute(net->route2Ds,sPin,ePin);  
+    TwoPinNet.route = FindRoute(ref,sPin,ePin);  
     
-    for (int i = rawRoute.size() - 1; i > 0; i--) {
-        line.sIdx = rawRoute[i];
-        line.eIdx = rawRoute[i - 1];
-        TwoPinNet.route.push_back(line);
-    }
     return TwoPinNet;
 }
-//not sure whether ref is good
-//this two part can be private
-vector<Pos> FindRoute (vector<Route2D> ref, Pos sPin, Pos ePin) {
-    vector<Pos> route;
+vector<Route2D> FindRoute (vector<Route2D>* ref, Pos sPin, Pos ePin) {
+    vector<Route2D> *rawRoute;
+    vector<Route2D> route;
+    Route2D line;
+    FindRouteRaw(rawRoute,ref,sPin,ePin);
+    for (int i = 0; i < rawRoute->size() - 1; i++) {
+        line.sIdx = rawRoute->at(i).sIdx;
+        line.eIdx = rawRoute->at(i + 1).sIdx;
+        route.push_back(line);
+    }
+    return route;
+}
+void FindRouteRaw (vector<Route2D>* route, vector<Route2D>* ref, Pos sPin, Pos ePin) {
     Pos end;
     Pos start;
     Pos Pin;
     Route2D line;
-    vector<Route2D> nextRef = ref;
     char achivable;
+    bool isLineUsed;
 
-    if (sPin.col == ePin.col && sPin.row == ePin.row) {
-        route.push_back(ePin);
-        return route;
+    if (isPosSame(sPin,ePin)) {
+        line.sIdx = sPin;
+        line.eIdx = ePin;
+        route->push_back(line);
+        return;
     }
 
-    for (int j = 0; j < ref.size(); j++) {
-        line = ref[j];
+    for (int j = 0; j < ref->size(); j++) {
+        isLineUsed = false;
+
+        line = ref->at(j);
         end = line.eIdx;
         start = line.sIdx;
 
-        achivable = isPininRoute(end, start, sPin);
+        // test whether line is in route already
+        for (auto usedline: *route) {
+            if (isPosSame(end,usedline.eIdx) && isPosSame(start,usedline.sIdx)) {
+                isLineUsed = true;
+            }
+        }
+        if (isLineUsed)
+            continue;
 
+        //line not used, then store it first
+        route->push_back(line);
+
+        //whether sPin in this line
+        achivable = isPininRoute(end, start, sPin);
+        //if in the line, then find next line
         if (achivable == 'v') {
             Pin.col = start.col;
             for (int i = min(end.row, start.row); i <= max(end.row, start.row); i++){
-                nextRef.erase(nextRef.begin() + j);
                 Pin.row = i;
-                route = FindRoute(nextRef, Pin, ePin);
-                nextRef = ref;
-                if (!route.empty()) {
-                    route.push_back(sPin);
-                    return route;
-                }
+                FindRouteRaw(route, ref, Pin, ePin);
             }
         } else if (achivable == 'h') {
             Pin.row = start.row;
             for (int i = min(end.col, start.col); i <= max(end.col, start.col); i++){
-                nextRef.erase(nextRef.begin() + j);
                 Pin.col = i;
-                route = FindRoute(nextRef, Pin, ePin);
-                nextRef = ref;
-                if (!route.empty()) {
-                    route.push_back(sPin);
-                    return route;
-                }
+                FindRouteRaw(route, ref, Pin, ePin);
             }
         }
+        //if line can't lead to ePin -> the line is not solution
+        if (!isPosSame(route->back().eIdx,ePin)) {
+            route->pop_back();
+        } else {
+            break;
+        }
     }
-    return vector<Pos>();
+    return;
+    // return vector<Pos>();
 }
+
 char isPininRoute(Pos end, Pos start, Pos Pin) {
     if (end.col == start.col) {
         if (min(end.row,start.row) <= Pin.row && Pin.row <= max(end.row,start.row))
@@ -196,3 +218,55 @@ char isPininRoute(Pos end, Pos start, Pos Pin) {
     }
     return '\0';
 }
+
+//not sure whether ref is good
+//this two part can be private
+// vector<Pos> FindRoute_old (vector<Route2D> ref, Pos sPin, Pos ePin) {
+//     vector<Pos> route;
+//     Pos end;
+//     Pos start;
+//     Pos Pin;
+//     Route2D line;
+//     vector<Route2D> nextRef = ref;
+//     char achivable;
+// 
+//     if (sPin.col == ePin.col && sPin.row == ePin.row) {
+//         route.push_back(ePin);
+//         return route;
+//     }
+// 
+//     for (int j = 0; j < ref.size(); j++) {
+//         line = ref[j];
+//         end = line.eIdx;
+//         start = line.sIdx;
+// 
+//         achivable = isPininRoute(end, start, sPin);
+// 
+//         if (achivable == 'v') {
+//             Pin.col = start.col;
+//             for (int i = min(end.row, start.row); i <= max(end.row, start.row); i++){
+//                 nextRef.erase(nextRef.begin() + j);
+//                 Pin.row = i;
+//                 route = FindRoute(nextRef, Pin, ePin);
+//                 nextRef = ref;
+//                 if (!route.empty()) {
+//                     route.push_back(sPin);
+//                     return route;
+//                 }
+//             }
+//         } else if (achivable == 'h') {
+//             Pin.row = start.row;
+//             for (int i = min(end.col, start.col); i <= max(end.col, start.col); i++){
+//                 nextRef.erase(nextRef.begin() + j);
+//                 Pin.col = i;
+//                 route = FindRoute(nextRef, Pin, ePin);
+//                 nextRef = ref;
+//                 if (!route.empty()) {
+//                     route.push_back(sPin);
+//                     return route;
+//                 }
+//             }
+//         }
+//     }
+//     return vector<Pos>();
+// }
