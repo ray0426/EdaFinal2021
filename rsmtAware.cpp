77b6f50110336cf224bbffec06e44392e2f18685
ray0426 <ray0426you@gmail.com>
2021-06-29 09:55:58 +0000

    pins process

--- rsmtAware.cpp
+++ rsmtAware.cpp
@@ -1,13 +1,56 @@
 #include <iostream>
 #include <vector>
 #include "flute.h"
+#include <algorithm>
 using namespace std;
+
+struct Pos {
+    int row;
+    int col;
+};
+
+struct Route2D {
+    Pos sIdx;
+    Pos eIdx;
+};
+
+struct Net2D {
+    vector<Pos> pin2Ds;
+    string name;
+    float weight;
+    vector<Route2D> route2Ds;
+};
 
 void rsmtAware(void);
 int wirelength(vector<int> powv, vector<int> hv);
+bool samePos(Pos pos1, Pos pos2);
+bool sortRow(Pos pos1, Pos pos2);
+void print(vector<Pos>& poss);
 
 int main(void) {
-    rsmtAware();
+    int i, j;
+    vector<Pos> pins = {
+        {2, 0},
+        {1, 4},
+        {0, 2},
+        {4, 3},
+        {1, 4},
+        {0, 2}
+    };
+    vector<Pos>::iterator it;
+    vector<int> h;
+    vector<int> v;
+
+    print(pins);
+    sort(pins.begin(), pins.end(), sortRow);
+    it = unique(pins.begin(), pins.end(), samePos);
+    pins.resize(distance(pins.begin(), it));
+    sort(pins.begin(), pins.end(), sortRow);
+    print(pins);
+
+    for (i = 0; i < pins.size(); i++) {
+
+    //rsmtAware();
     return 0;
 }
 
@@ -29,4 +72,24 @@ int wirelength(vector<int> powv, vector<int> hv) {
         result += powv[i] * hv[i];
     }
     return result;
+}
+
+
+bool samePos(Pos pos1, Pos pos2) {
+    return (pos1.row == pos2.row) && (pos1.col == pos2.col);
+}
+
+bool sortRow(Pos pos1, Pos pos2) {
+    return pos1.row <= pos2.row;
+}
+
+void print(vector<Pos>& poss) {
+    int i;
+
+    cout << "pins:" << endl;
+    for (i = 0; i < poss.size(); i++) {
+        cout << "(" << poss[i].row << ", " <<
+            poss[i].col << ")" << endl;
+    }
+    cout << endl;
 }
