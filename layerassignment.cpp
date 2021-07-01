102639ccd062c809667f9cfd8275378c58563260
jminglee <jimmy0401173@gmail.com>
2021-07-01 17:30:34 +0800

    layerassignment debug finish

--- layerassignment.cpp
+++ layerassignment.cpp
@@ -1,84 +1,24 @@
-//#ifndef LAYERASSIGNMENT_H
-//#define LAYERASSIGNMENT_H
-
 #include "problem.h"
-#include "compress.h"
 #include <bits/stdc++.h>
 using namespace std;
 
-// 2D 格子點
-typedef struct vertex2d la_Vertex2d;
-// edge
-typedef struct edge la_Edge;
-// 2D net from previous process
-typedef struct net2d la_Net2d;
-// 3D wire
-typedef struct wire la_Wire;
-// 3D net for output
-typedef struct net3d la_Net3d;
-// final route
-typedef struct route la_Route;
+void _set_vertex(la_Vertex2d *v, int r, int c, int p, bool i, Problem *pro);
+int _find_id_in_vector(vector<Pos> v, Pos t);
+la_Vertex2d *_find_vertex(vector<la_Vertex2d*> vs, int row, int col);
 
-struct vertex2d
-{ 
-    int row, col, layer, color, pin_layer; 
-    bool is_pin; 
-    vector<la_Vertex2d*> adjlist;
-    la_Vertex2d* parent;
-    vector<la_Vertex2d*> child;
-    la_Edge* parent_edge;
-    vector<vector<int>> child_layer;
+vector<la_Net2d> _transform(vector<Net2D> net, Problem *p);
 
-    void init(int r, int c, int p, bool i)
-    {
-        row = r; col = c; layer = -1; color = 0; pin_layer = p; is_pin = i;
-        parent = NULL; parent_edge = NULL; 
-    }
+void _quicksort(vector<la_Net2d> &net, vector<double> &score, int l, int r);
+void _net_ordering(vector<la_Net2d> &net2d);
 
-    la_Vertex2d operator=(la_Vertex2d a)
-    {
-        row = a.row; col = a.col; layer = a.layer; color = a.color; pin_layer = a.pin_layer; is_pin = a.is_pin;
-        adjlist = a.adjlist; parent = a.parent; child = a.child; parent_edge = a.parent_edge; child_layer = a.child_layer;
-        return a;
-    }
-};
+void _DFS_visit(la_Vertex2d *u, vector<la_Vertex2d*> &dft);
+vector<la_Vertex2d*> _net_preprocessing(la_Net2d net);
 
-struct edge
-{ 
-    la_Vertex2d *fir, *sec; 
+float _minimum_via_cost(la_Vertex2d *v, int layer, int minLay, Problem *p, 
+char *dir, vector<vector<vector<float>>> supply, vector<vector<vector<float>>> demand);
 
-    char _dir(){ return (fir->row == sec->row)?'H':'V'; }
-    la_Edge operator=(la_Edge a){ fir = a.fir; sec = a.sec; return a; }
-    void print(){ printf("(%d,%d)->(%d,%d)", fir->row, fir->col, sec->row, sec->col); return; }
-};
-
-struct net2d
-{
-    int id;
-    vector<la_Vertex2d*> vertex; 
-    vector<la_Edge*> edge; 
-    double weight;
-    int minRouteLayConst;
-
-    double _score()
-    {
-        int total_length = edge.size(), pinnum = 0;
-        for(auto a : vertex) if(a->is_pin) pinnum++;
-        return 1/total_length+pinnum;
-    }
-};
-
-struct wire
-{ 
-    int s_row, s_col, s_layer, e_row, e_col, e_layer; 
-    void _make_wire(la_Vertex2d *x, la_Vertex2d *y)
-    {
-        s_row = x->row; s_col = x->col; s_layer = x->layer;
-        e_row = y->row; e_col = y->col; e_layer = y->layer;
-    }
-};
-struct net3d{ vector<la_Wire*> wires; };
-struct route{ vector<la_Net3d*> nets; };
+void _route_print(la_Route r, Problem *p);
+void _layer_assignment_and_print_route(Problem *p, vector<Net2D> &net);
 
 int _find_id_in_vector(vector<Pos> v, Pos t)
 {
@@ -345,13 +285,3 @@ void _layer_assignment_and_print_route(Problem p, vector<Net2D> net)
     _route_print(r, p);
 }
 
-//#endif
-
-int main(int argc, char **argv)
-{
-    Problem *pro = new Problem;
-    pro->readCase(argv[1]);
-    vector<Net2D> net =  Three2Two(pro);
-    vector<la_Net2d*> net2d = _transform(net);
-    return 0;
-}
